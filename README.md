a232ec0abc652e2f4c25845aa95f4ef55d88e23f
DC <curtis.deshawn@ymail.com>
2025-03-30 07:30:45 +0000

    latest attempt having trouble with console.log

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # madLibExercise
+**In this assignment I am using a story to create elements and data types in javascript.**
--- madLibs.js
+++ madLibs.js
@@ -14,25 +14,34 @@ const booleanValue = timeOfDay.includes("morning") ;
 
 //Object with properties 
 
-const boat = { name: "Black Pearl"};
+const ship = { name: "Black Pearl" };
 
 //property of the object 
 
-const object1Owner = boat.name;
+const BoatName = ship.name; 
+
+
 
 
 //Story
 
 
 
-const story =  ` One bright morning, I set sail on a pirate ship named The Black Pearl. 
-The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in 
-the water. 'Could it be treasure?' the captain exclaimed. We hauled it aboard and opened it. 
-Did you know 5 × 5 equals 25? Incredible, right? ` ;  
-
+const story =  ` 2. The Pirate Adventure
+"One bright morning, I set sail on a pirate ship named The Black Pearl. 
+The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in the water. 
+'Could it be treasure?' the captain exclaimed. We hauled it aboard and opened it. Did you know 5 × 5 equals 25? 
+Incredible, right?" `;
+document.getElementById("madlibs-output").textContent = story;
+  
 
 
 
 
 
 console.log(story);
+
+
+
+
+
