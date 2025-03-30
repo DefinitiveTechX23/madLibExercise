181ec01b2a99ee6223c61e3c0ad4710368fcab25
DC <curtis.deshawn@ymail.com>
2025-03-30 06:51:40 +0000

    1st attempt

---
+++ madLibs.js
@@ -0,0 +1,38 @@
+
+
+//Stirngs
+const timeOfDay = "one bright morning";
+const location = "sea";
+const mystery = "chest";
+const pirate = "captain";
+
+//numbers
+const multi = 5 * 5;
+
+//boolean
+const booleanValue = timeOfDay.includes("morning") ;
+
+//Object with properties 
+
+const boat = { name: "Black Pearl"};
+
+//property of the object 
+
+const object1Owner = boat.name;
+
+
+//Story
+
+
+
+const story =  ` One bright morning, I set sail on a pirate ship named The Black Pearl. 
+The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in 
+the water. 'Could it be treasure?' the captain exclaimed. We hauled it aboard and opened it. 
+Did you know 5 × 5 equals 25? Incredible, right? ` ;  
+
+
+
+
+
+
+console.log(story);
