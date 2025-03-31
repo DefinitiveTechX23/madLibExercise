244e57c27cd77bfd2b10eac1b8f9204d099e15fc
DC <curtis.deshawn@ymail.com>
2025-03-31 21:13:19 +0000

    took out location as a vaariable name

--- madLibs.js
+++ madLibs.js
@@ -1,15 +1,21 @@
 
 
-//Stirngs
+//Strings
+
+
 const timeOfDay = "one bright morning";
-const location = "sea";
+const TravelDestination = "sea";
 const mystery = "chest";
 const pirate = "captain";
 
 //numbers
+
+
 const multi = 5 * 5;
 
 //boolean
+
+
 const booleanValue = timeOfDay.includes("morning") ;
 
 //Object with properties 
@@ -32,14 +38,17 @@ const story =  ` 2. The Pirate Adventure
 The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in the water. 
 'Could it be treasure?' the captain exclaimed. We hauled it aboard and opened it. Did you know 5 × 5 equals 25? 
 Incredible, right?" `;
+
+
+
+
+
 document.getElementById("madlibs-output").textContent = story;
-  
-
-
-
 
 
 console.log(story);
+
+
 
 
 
